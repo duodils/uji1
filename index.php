da3cea5953ac7d86dfcbb4f7ba2e72b5e5461fa6
duodils <epimpim8@gmail.com>
2024-08-28 11:54:52 +0700

    Menambahkan Home

--- index.php
+++ index.php
@@ -4,27 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-    <!-- navbar -->
+    <div class="container">
+        <!-- navbar -->
+        <?php require ('includes/navbar.php') ?>
+        <!-- home -->
+        <div class="home" id="home">
+            <img class="bg-home" src="assets/images/bg.jfif" alt="">
+                <div class="content-left">
+                    <img class="bruno" src="assets/images/bruno.png" alt="">
+                </div>
+                <div class="content-right" style="">
+                    <h1 class="subtitle">COMMING SOON</h1>
+                    <H2 class="title">Bruno On Stage</H2>
+                    <p class="location">Jakarta Intenational Stadium</p>
+                    <div class="informasi">
+                        <p>Tour for the Latest Album - '24K Magic'.</p>
+                        <p>Sunday, September 15, 2024</p>   
+                    </div>
+                </div>
+        </div>
+
+        <!-- about -->
 
 
-    
-    <!-- home -->
+        <!-- ticket -->
 
 
-    <!-- about -->
+        <!-- layout -->
 
 
-    <!-- ticket -->
+        <!-- galery -->
 
 
-    <!-- layout -->
-
-
-    <!-- galery -->
-
-
-    <!-- footer -->
+        <!-- footer -->
+    </div>
 </body>
 </html>
