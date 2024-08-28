85a5fddeba199e6cdeb830984f40cbb6454eeac5
duodils <epimpim8@gmail.com>
2024-08-28 15:43:13 +0700

    Menambahkan navbar dan home

--- index.php
+++ index.php
@@ -12,19 +12,16 @@
         <?php require ('includes/navbar.php') ?>
         <!-- home -->
         <div class="home" id="home">
-            <img class="bg-home" src="assets/images/bg.jfif" alt="">
-                <div class="content-left">
-                    <img class="bruno" src="assets/images/bruno.png" alt="">
+            <img class="bg-home" src="assets/images/brunobg.png" alt="">
+            <div class="content-right" style="">
+                <h1 class="subtitle">COMMING SOON</h1>
+                <H2 class="title">Bruno On Stage</H2>
+                <p class="location">Jakarta Intenational Stadium</p>
+                <div class="informasi">
+                    <p>Tour for the Latest Album - '24K Magic'.</p>
+                    <p>Sunday, September 15, 2024</p>   
                 </div>
-                <div class="content-right" style="">
-                    <h1 class="subtitle">COMMING SOON</h1>
-                    <H2 class="title">Bruno On Stage</H2>
-                    <p class="location">Jakarta Intenational Stadium</p>
-                    <div class="informasi">
-                        <p>Tour for the Latest Album - '24K Magic'.</p>
-                        <p>Sunday, September 15, 2024</p>   
-                    </div>
-                </div>
+            </div>
         </div>
 
         <!-- about -->
